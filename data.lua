6baf6d0dd600de8aab841e88fd21dc114aedcbcd
nidavellir-crypto <seeinghershadowfade@gmail.com>
2026-04-19 11:36:11 +0300

    Create data.lua

---
+++ data.lua
@@ -0,0 +1,50 @@
+_G.NidavellirData = {}
+
+_G.NidavellirData.WEAPON_SKINS = {
+    ["Deagle gold"] = { ColorMap = "rbxassetid://9422465914", MetalnessMap = "rbxassetid://9422467019", NormalMap = "rbxassetid://9368506864", RoughnessMap = "rbxassetid://9422469052" },
+    ["M4A1-1 PATRIOT"] = { ColorMap = "rbxassetid://13945985275", MetalnessMap = "rbxassetid://13945986111", NormalMap = "rbxassetid://13945989865", RoughnessMap = "rbxassetid://13945990897" },
+    ["M4A1-1 MELT"] = { ColorMap = "rbxassetid://138263153339452", MetalnessMap = "rbxassetid://122701374554312", NormalMap = "rbxassetid://92873024476028", RoughnessMap = "rbxassetid://77602151073625" },
+    ["Mare gold"] = { ColorMap = "rbxassetid://16560241772", MetalnessMap = "rbxassetid://8969034988", NormalMap = "rbxassetid://8969047074", RoughnessMap = "rbxassetid://8969056777" },
+    ["MP7 hell"] = { ColorMap = "rbxassetid://13842812065", MetalnessMap = "rbxassetid://13841892618", NormalMap = "rbxassetid://13841893577", RoughnessMap = "rbxassetid://13841894868" },
+    ["Uzi smiley"] = { ColorMap = "rbxassetid://13841666943", MetalnessMap = "rbxassetid://13841666943", NormalMap = "rbxassetid://13841669834", RoughnessMap = "rbxassetid://13715505740" },
+    ["Beretta gold"] = { ColorMap = "rbxassetid://15071881699", MetalnessMap = "rbxassetid://15071881490", NormalMap = "rbxassetid://15071881251", RoughnessMap = "rbxassetid://15071880826" },
+    ["AKS-74U cherry"] = { ColorMap = "rbxassetid://16355374224", MetalnessMap = "rbxassetid://16355374709", NormalMap = "rbxassetid://16355375244", RoughnessMap = "rbxassetid://16355375483" },
+    ["SKS jester"] = { ColorMap = "rbxassetid://13343167267", MetalnessMap = "rbxassetid://13343167958", NormalMap = "rbxassetid://13343168956", RoughnessMap = "rbxassetid://13343169804" },
+    ["M1911 glory"] = { ColorMap = "rbxassetid://13948805827", MetalnessMap = "rbxassetid://13948290980", NormalMap = "rbxassetid://13948291596", RoughnessMap = "rbxassetid://13948321939" },
+    ["Sawn-Off glacial"] = { ColorMap = "rbxassetid://13030805318", MetalnessMap = "rbxassetid://13030809551", NormalMap = "rbxassetid://13030811843", RoughnessMap = "rbxassetid://13030812846" },
+    ["G-17 elim"] = { ColorMap = "rbxassetid://94164067871562", MetalnessMap = "rbxassetid://133337669834230", NormalMap = "rbxassetid://75074216652378", RoughnessMap = "rbxassetid://112379003610843" },
+    ["Deagle nacho"] = { ColorMap = "rbxassetid://16942393059", MetalnessMap = "rbxassetid://13937635046", NormalMap = "rbxassetid://13935410021", RoughnessMap = "rbxassetid://13935410676" },
+    ["SKS jacko"] = { ColorMap = "rbxassetid://15177197176", MetalnessMap = "rbxassetid://15177198090", NormalMap = "rbxassetid://15177203653", RoughnessMap = "rbxassetid://15177204339" },
+    ["Sawn-Off grandprix"] = { ColorMap = "rbxassetid://13841748041", MetalnessMap = "rbxassetid://13841749691", NormalMap = "rbxassetid://13841750446", RoughnessMap = "rbxassetid://13556254787" },
+    ["UMP-45 daejing"] = { ColorMap = "rbxassetid://125942364208210", MetalnessMap = "rbxassetid://98525310658486", NormalMap = "rbxassetid://80254908603579", RoughnessMap = "rbxassetid://112502290266633" },
+    ["Magnum amour"] = { ColorMap = "rbxassetid://16355307379", MetalnessMap = "rbxassetid://16355307601", NormalMap = "rbxassetid://16355307807", RoughnessMap = "rbxassetid://16355308060" },
+    ["Mare trickshot"] = { ColorMap = "rbxassetid://16907785827", MetalnessMap = "rbxassetid://16907786165", NormalMap = "rbxassetid://16907787618", RoughnessMap = "rbxassetid://16907788208" },
+    ["MAC-10 cryofox"] = { ColorMap = "rbxassetid://72676229182235", MetalnessMap = "rbxassetid://99722143158841", NormalMap = "rbxassetid://98358790768254", RoughnessMap = "rbxassetid://75331173382652" },
+    ["G-17 yosei"] = { ColorMap = "rbxassetid://16040462957", MetalnessMap = "rbxassetid://16040463384", NormalMap = "rbxassetid://16040464100", RoughnessMap = "rbxassetid://16040464355" }
+}
+
+_G.NidavellirData.Outfits = {
+    {"FEM", "90496496670985", "87434096937281"}, {"LEIFEN", "128102091001221", "140100566671121"},
+    {"DRACULA", "71375126050262", "131044823890502"}, {"TRIPPY", "123744182593552", "105322170847516"},
+    {"RED STRIPES", "78347653749174", "125430867704474"}, {"ANTIQUE", "96793954857348", "81877474271701"},
+    {"DEMONFLAKES", "131922256719420", "94846149811131"}, {"ROME GOD", "140685532390320", "119735736161089"},
+    {"BEFORE DEATH", "81154586296712", "122118071807542"}, {"PURPLE DEMON", "107262757428348", "81567517325807"},
+    {"DVD", "104874114862744", "87082194701554"}, {"CRUELINT VKEI WHITE", "83551828914015", "79458459663722"},
+    {"WHEELCHAIR", "81897251918843", "89246765410359"}, {"RVVZ", "94464650073587", "105497350428965"},
+    {"FOREST", "77071225404367", "118465170888147"}, {"MOE BALENCIAGA", "100098811070017", "113033250162217"},
+    {"ADIDAS TRACKSUIT", "77402994780831", "72513998118906"}, {"DEPOP", "92901296879272", "132595825036540"},
+    {"SUITED", "136662779245300", "73542344598169"}, {"ROSE", "137115475659158", "129466348077233"},
+    {"PONY", "97618859359149", "125138942037128"}, {"SANDY", "88741516199307", "94972754289125"},
+    {"GALAXY ADIDAS", "84809554070738", "79588119227965"}, {"RAWR", "74403912004029", "114178472288901"},
+    {"地獄 SIGMA", "3358112084710", "128880023035051"}, {"BLUE HOMELESS", "88638115054997", "135007731735256"},
+    {"DORITOS", "102459479324189", "130311926249168"}, {"POU", "134546161609112", "128666645981500"},
+    {"FROSTBITE", "134055702630915", "83555047265328"}, {"ALIEN F", "136995518396655", "127620980866191"},
+    {"BUTTONED UP", "103038498084850", "111544493105311"}, {"WHITE MONSTER", "82108126056056", "111449789978533"},
+    {"ARAB", "72040208556702", "118163074760282"}, {"SPECIAL", "94390390065006", "78791761028036"},
+    {"SPONGEBOB", "115108052637079", "137343055808841"}
+}
+
+_G.NidavellirData.Skins = {
+    {"PALE", Color3.fromRGB(253, 234, 212)}, {"LIGHT", Color3.fromRGB(246, 203, 175)}, {"MEDIUM", Color3.fromRGB(219, 153, 114)},
+    {"TAN", Color3.fromRGB(180, 113, 76)}, {"DARK", Color3.fromRGB(124, 76, 50)}, {"DEEP", Color3.fromRGB(67, 40, 24)}, {"BLACK", Color3.fromRGB(0, 0, 0)}
+}
